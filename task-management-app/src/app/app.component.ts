2ba56bbebcb2cfc2b00a2ad00c158d48c57f9aba
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-08-26 19:20:01 +0300

    Exercise

--- task-management-app/src/app/app.component.ts
+++ task-management-app/src/app/app.component.ts
@@ -3,19 +3,27 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,UserComponent],
+  imports: [RouterOutlet,HeaderComponent,UserComponent,TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'task-management-app';
   users= DUMMY_USERS; 
+  selectedUser=this.users[0];
 
   onSelectUser(id: string){
-    console.log("selected user with id " + id);
+
+let obj=this.users.find(user => {
+  return user.id==id;
+});
+    if(obj != undefined){
+      this.selectedUser =obj;
+    } 
   }
 }
---
+++ task-management-app/src/app/task/task.component.ts
@@ -0,0 +1,19 @@
+import { Component,Input,Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  onSelectUser(){
+    this.select.emit(this.name);
+  }
+}
+
+
