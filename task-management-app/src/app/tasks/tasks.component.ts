1130adadb5bda6ed423521124302488fbe9f53a4
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-08-27 20:00:58 +0300

    Adding tasks to users

--- task-management-app/src/app/tasks/tasks.component.ts
+++ task-management-app/src/app/tasks/tasks.component.ts
@@ -1,10 +1,12 @@
 import { Component,Input,Output,EventEmitter } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -12,6 +14,8 @@ import { TaskComponent } from './task/task.component';
 export class TasksComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) name!: string;
+  isAddingTask = false; 
+
   tasks=[
     {
       id: 't1',
@@ -44,6 +48,24 @@ export class TasksComponent {
 
   onCompleteTask(id: string){
     this.tasks=this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask=false;
+  }
+
+  onAddTask(taskData: NewTaskData ){
+     this.tasks.push({
+     id: new Date().getTime().toString(),
+     userId: this.id,
+     dueDate: taskData.date,
+     title: taskData.title,
+     summary: taskData.summary
+  })
   }
 
 }
