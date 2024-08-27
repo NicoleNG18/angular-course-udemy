cd21ed93be123d19f1398688510cef273047919a
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-08-27 17:42:08 +0300

    NgFor and ngIf

--- task-management-app/src/app/app.component.ts
+++ task-management-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor,NgIf } from '@angular/common';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
@@ -8,7 +10,7 @@ import { TaskComponent } from './task/task.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,UserComponent,TaskComponent],
+  imports: [RouterOutlet,HeaderComponent,UserComponent,TaskComponent,NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
