ad9c64e25c18cfb5e6d911cf68bf049b7bae576b
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-08-28 20:54:43 +0300

    storing data in browser local storage

--- task-management-app/src/app/tasks/tasks.service.ts
+++ task-management-app/src/app/tasks/tasks.service.ts
@@ -30,6 +30,15 @@ export class TasksService {
         },
       ]
 
+      constructor(){
+        const tasks = localStorage.getItem('tasks');
+
+        if(tasks){
+            this.tasks=JSON.parse(tasks); 
+        }
+
+      }
+
       getUsersTask (userId: string){
          return this.tasks.filter((task) => task.userId == userId);
       }
@@ -42,10 +51,16 @@ export class TasksService {
             title: taskData.title,
             summary: taskData.summary
          });
+         this.saveTasks();
       }
 
       removeTask(id: string){
         this.tasks=this.tasks.filter((task) => task.id !== id);
+        this.saveTasks();
+      }
+
+      private saveTasks(){
+        localStorage.setItem('tasks',JSON.stringify(this.tasks));
       }
 
 }
