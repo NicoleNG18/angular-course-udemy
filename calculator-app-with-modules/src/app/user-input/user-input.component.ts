df9b4a8dfcecb529af693dcb018fb05368e932e9
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-09-02 12:56:49 +0300

    calculator app with modules

---
+++ calculator-app-with-modules/src/app/user-input/user-input.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+
+@Component({
+  selector: 'app-user-input',
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  constructor (private investmentService: InvestmentService){}
+
+enteredInitialInvestment = '0';
+enteredAnnualInvestment = '0';
+enteredExpectedReturn = '5';
+enteredDuration = '10';
+
+onSubmit(){
+this.investmentService.calculateInvestmentResults({
+  initialInvestment: + this.enteredInitialInvestment,
+  duration: + this.enteredAnnualInvestment,
+  expectedReturn: + this.enteredExpectedReturn,
+  annualInvestment: + this.enteredDuration,
+});
+
+   this.enteredInitialInvestment = '0';
+   this.enteredAnnualInvestment = '0';
+   this.enteredExpectedReturn = '5';
+   this.enteredDuration = '10';
+}
+
+}
