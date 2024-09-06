05b5c89251368c2f21a9caefd24b26be78905e87
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-09-06 17:22:46 +0300

    using class binding syntax

--- components-deep-dive-app/src/app/shared/control/control.component.ts
+++ components-deep-dive-app/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -12,5 +12,6 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   }
 })
 export class ControlComponent {
+    //  @HostBinding('class') className = 'control';
      @Input({required: true}) label!: string;
 }
